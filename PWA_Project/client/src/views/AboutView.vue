d2b2640c30c3e95b1ef6514ad23885f8a5968dfc
OnFeuerFrosch <104189562+Ruben-Muellauer-HTL@users.noreply.github.com>
2024-03-05 17:05:24 +0100

    about Page

--- PWA_Project/client/src/views/AboutView.vue
+++ PWA_Project/client/src/views/AboutView.vue
@@ -1,1 +1,55 @@
-<template>About</template>
+<template class="bg-secondary">
+  <div class="text-center q-pt-xl">
+    <span class="text-h3 text-center">Welcome to GlobeVista!</span>
+    <div class="row justify-center">
+      <q-card style="width: 20rem" class="q-ma-xl">
+        <q-img src="../assets/about/why.jpg" />
+
+        <q-card-section>
+          <div class="text-h6">Warum Wir?</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <span class="ellipsis-3-lines">
+            Tauchen Sie ein in neue Kul"> Von maßgeschneiderten Abenteuern bis zu romantischen
+            Fluchten GlobeVista zeichnet sich durch Liebe zum Detail, Engagement für Qualität und
+            persönlichen Service aus. Ihre Zufriedenheit steht bei uns an erster Stelle.
+          </span>
+          <q-btn color="info" class="q-mt-md"><span class="text-dark">Explore more...</span></q-btn>
+        </q-card-section>
+      </q-card>
+      <q-card style="width: 20rem" class="q-ma-xl">
+        <q-img src="../assets/about/explore.jpg" />
+
+        <q-card-section>
+          <div class="text-h6">Entdecken Sie Mit Uns</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <span class="ellipsis-3-lines">
+            Tauchen Sie ein in neue Kulturen, atemberaubende Landschaften und unvergessliche
+            Begegnungen. GlobeVista öffnet Türen zu faszinierenden Destinationen, während Sie sich
+            auf unbeschwerte Reiseerlebnisse freuen dürfen.
+          </span>
+          <q-btn color="info" class="q-mt-md"><span class="text-dark">Explore more...</span></q-btn>
+        </q-card-section>
+      </q-card>
+      <q-card style="width: 20rem" class="q-ma-xl">
+        <q-img src="../assets/about/mission.jpg" />
+
+        <q-card-section>
+          <div class="text-h6">Unsere Mission</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <span class="ellipsis-3-lines">
+            Wir bei GlobeVista setzen uns leidenschaftlich dafür ein, einzigartige Reiseerlebnisse
+            zu schaffen. Entdecken Sie mit uns die Welt auf eine Weise, die Ihre Träume wahr werden
+            lässt.
+          </span>
+          <q-btn color="info" class="q-mt-md"><span class="text-dark">Explore more...</span></q-btn>
+        </q-card-section>
+      </q-card>
+    </div>
+  </div>
+</template>
--- PWA_Project/client/src/views/HomeView.vue
+++ PWA_Project/client/src/views/HomeView.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="column items-center bg-primary text-white justify-center" style="height: 20vh">
+  <div class="column items-center bg-primary text-white justify-center" style="height: 30vh">
     <span class="text-h4">GlobeVista</span>
     <span class="q-mt-md">Explore Beyond Boundaries: Your Journey, Your Way!</span>
     <q-btn class="bg-positive text-dark q-mt-md" style="width: 10rem" to="/travel"
       >Travel Now</q-btn
     >
   </div>
-  <q-img src="../assets/background.jpg" style="height: 80vh"></q-img>
+  <q-img src="../assets/background.jpg" style="height: 70vh"></q-img>
 </template>
